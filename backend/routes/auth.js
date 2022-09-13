eb00fe9c4bf8811b716a366be6f8d2bb5abf61ac
sanjay <sanjaysewa999@gmail.com>
2022-09-13 21:24:12 +0530

    validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../mongoose_module/Users');
+const User = require("../mongoose_module/Users");
+const { body, validationResult } = require("express-validator");
 
-// Create a user using POST method --- End Point is --> /api/auth
+// Create a user using POST method --- End Point is --> /api/auth/createuser
 
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    console.log(req.body);
-    res.send(req.body);
-});
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name.").isLength({ min: 3 }),
+    body("email", "Enter a valid email.").isEmail(),
+    body("password", "Password must be atleast 5 character.").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    // If there are error return Bad request
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let userEmail = await User.findOne({ email: req.body.email });
+
+      if (userEmail) {
+        return res.status(400).json({ error: "Email alresy exists." });
+      }
+
+      let users = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.send(users);
+    } catch (error) {
+      res.status(500).json({ error: "Some error occured" });
+    }
+  }
+);
 
 module.exports = router;
